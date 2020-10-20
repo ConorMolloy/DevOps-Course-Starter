0fffb790b96747c7c9ef120f747e3449aeffa436
Conor Molloy <from.conor.molloy@gmail.com>
2020-10-20 09:29:44 +0100

    Change class name in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import session_items as session
+import trello_client as session
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
