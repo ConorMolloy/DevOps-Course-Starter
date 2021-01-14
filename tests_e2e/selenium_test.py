f45b203848f1b7efe29cf4045e12546be05970ac
Conor Molloy <from.conor.molloy@gmail.com>
2021-01-14 08:50:12 +0000

    Expand on selenium test

--- tests_e2e/selenium_test.py
+++ tests_e2e/selenium_test.py
@@ -1,5 +1,7 @@
+from datetime import time
 import pytest
 import app
+import time
 from selenium import webdriver
 from threading import Thread
 
@@ -7,6 +9,7 @@ from threading import Thread
 def test_app():
     # construct the new application
     application = app.create_app()
+    time.sleep(1)
 
     # start the app in its own thread.
     thread = Thread(target=lambda: application.run(use_reloader=False)) 
@@ -23,5 +26,13 @@ def driver():
         yield driver
 
 def test_task_journey(driver, test_app): 
+    test_item_name = 'Finish selenium test'
     driver.get('http://localhost:5000/')
+    driver.find_element_by_id('item').send_keys(test_item_name)
+    driver.find_element_by_id('submit').click()
+    time.sleep(1)
+    driver.find_element_by_id(test_item_name+'_complete').click()
+    time.sleep(1)
+    driver.find_element_by_id(test_item_name+'_delete').click()
+
     assert driver.title == 'To-Do App'
