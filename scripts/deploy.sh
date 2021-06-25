23a30421330221d520ac9f20cab2219daed32c8b
Conor Molloy <from.conor.molloy@gmail.com>
2021-06-25 08:37:01 +0100

    Which hosting from heroku to azure

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
 docker build --target production --tag "$DOCKER_USERNAME"/todo-app:"$TRAVIS_COMMIT" .
+docker tag "$DOCKER_USERNAME"/todo-app:"$TRAVIS_COMMIT" "$DOCKER_USERNAME"/todo-app
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "$DOCKER_USERNAME"/todo-app:"$TRAVIS_COMMIT"
-docker tag "$DOCKER_USERNAME"/todo-app:"$TRAVIS_COMMIT" registry.heroku.com/devops-starter/web
-echo "$HEROKU_API_KEY" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
-docker push registry.heroku.com/devops-starter/web
-heroku container:release web --app=devops-starter
+docker push "$DOCKER_USERNAME"/todo-app
+curl -dH -X POST "$AZURE_WEBHOOK"
